0912f44d91dcc0fb58eea14df79c36bcca630efc
ADMIN <ADMIN@LAB1-MAY21>
2023-09-25 16:47:58 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace CSharp_dethithu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.lblInfo.Location = new System.Drawing.Point(70, 102);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(309, 31);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "THÔNG TIN SINH VIÊN";
+            this.lblInfo.Click += new System.EventHandler(this.lblInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(973, 533);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "Form1";
+            this.Text = "Đề Thi Cử";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_dethithu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblInfo_Click(object sender, EventArgs e)
+        {
+            string hovaten = "Nguyen Hoang Anh";
+            string mssv = "123456789";
+            string monthi = "Lap trinh Window 2 - c#";
+
+            lblInfo.Text = " ho va ten: " + hovaten;
+            lblInfo.Text += "\n mssv: " + mssv;
+            lblInfo.Text += "\n ngay thi: " + System.DateTime.Now.ToString();
+            lblInfo.Text += "\n mon thi: " + monthi;
+        }
+    }
+}
